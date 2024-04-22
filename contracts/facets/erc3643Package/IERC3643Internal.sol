2832c587b350977ea50caee741971a7c0dfe4e44
YASSINE EL BACHIRI <yassineelbachiri21@gmail.con>
2024-04-22 16:53:19 +0300

    chore: remove existing erc3643Package

--- contracts/facets/erc3643Package/IERC3643Internal.sol
+++ contracts/facets/erc3643Package/IERC3643Internal.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+pragma experimental ABIEncoderV2;
 
 interface IERC3643Internal {
     // Enums
@@ -24,6 +25,10 @@ interface IERC3643Internal {
     event TokensUnfrozen(address indexed user, uint256 amount);
     event WalletFrozen(address indexed user);
     event WalletUnfrozen(address indexed user);
-    
-    
+    event TokenPaused(bool paused);
+    event ComplianceContractSet(address indexed complianceContract);
+    event OnchainIDUpdated(address indexed newOnchainID);
+    event IdentityVerificationUpdated(address indexed user, bool isVerified);
+    event WalletFrozenStatusUpdated(address indexed user, bool isFrozen);
+    event TransferERC3643ComplianceCheck(address indexed from, address indexed to, uint256 value, ComplianceCheck complianceCheck);
 }
